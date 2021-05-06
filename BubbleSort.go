eb51d824732b9890bcb132d72697f5f2d0dbdbfb
Bmegaminds <gyan.cloudpobox@gmail.com>
2021-05-06 18:00:09 +0530

    Add files via upload

---
+++ BubbleSort.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var numbers = []int{5, 6, 8, 10, 23, 6, 7, 7, 7}
+	var i, j int
+
+	for i = 0; i < len(numbers)-1; i++ {
+		for j = i + 1; j < len(numbers); j++ {
+			if numbers[i] > numbers[j] {
+				var temp = numbers[i]
+				numbers[i] = numbers[j]
+				numbers[j] = temp
+			}
+		}
+	}
+
+	for j = 0; j < len(numbers); j++ {
+		fmt.Printf("%d,", numbers[j])
+	}
+}
